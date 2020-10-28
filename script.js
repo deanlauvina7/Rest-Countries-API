648a4cae7f81005fb26fcf7394790b033994b5c3
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-28 07:07:16 -0500

    Created the filter by region button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const countriesEl = document.getElementById('countries');
 const toggleBtn = document.getElementById('toggle');
 const filterBtn = document.getElementById('filter');
+const regionFilters = document.querySelectorAll('li');
+const searchEl = document.getElementById('search');
 
 
 // functions
@@ -24,9 +26,9 @@ function displayCountries(countries) {
         <img src="${country.flag}" alt="${country.name}">
       </div>
       <div class="card-body">
-        <h2>${country.name}</h2>
+        <h2 class="country-name">${country.name}</h2>
         <p><strong>Population: </strong>${country.population}</p>
-        <p><strong>Region: </strong>${country.region}</p>
+        <p class="country-region"><strong>Region: </strong>${country.region}</p>
         <p><strong>Capital: </strong>${country.capital}</p>
               </div>
   `;
@@ -34,10 +36,41 @@ function displayCountries(countries) {
   });
 };
 
+// toggle theme 
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
 
+// show and hide the filters
 filterBtn.addEventListener('click', () => {
   filterBtn.classList.toggle('open');
+});
+
+// search for a country
+searchEl.addEventListener('input', (e) => {
+  const val = e.target.value;
+  const countryName = document.querySelectorAll('.country-name');
+
+  countryName.forEach(name => {
+    if (name.innerText.toLowerCase().includes(val.toLowerCase())) {
+      name.parentElement.parentElement.style.display = 'block';
+    } else {
+      name.parentElement.parentElement.style.display = 'none';
+    }
+  });
+});
+
+// filter out countries by part of world
+regionFilters.forEach(filter => {
+  filter.addEventListener('click', () => {
+    const countryRegion = document.querySelectorAll('.country-region');
+
+    countryRegion.forEach(region => {
+      if (region.innerText.toLowerCase().includes(filter.innerText.toLowerCase())) {
+        region.parentElement.parentElement.style.display = 'block';
+      } else {
+        region.parentElement.parentElement.style.display = 'none';
+      }
+    });
+  });
 });
